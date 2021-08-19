3282c8231149bd8e606b1aa6f99e3f483fb229e3
JoseRoman <48696141+romanfjose@users.noreply.github.com>
2021-08-19 01:29:05 -0300

    Agregar formato al archivo

---
+++ Roman-Converter.js
@@ -0,0 +1,72 @@
+function convertToRoman(num) {
+  let roman = "";
+  while(num >= 1){
+    
+ 
+    if(roman.includes("III")){
+     if(roman.includes( "VIII")){
+       
+         roman = roman.replace("VIII", "IX");  }
+      else  
+      roman = roman.replace("III", "IV");
+                }
+       else
+        if(roman.includes("IV")){
+          roman = roman.replace("IV", "V");} 
+        else
+        if(roman.includes("IX")){
+          roman = roman.replace ("IX", "X");}
+          else
+          roman = roman +"I";
+          num--; 
+  
+ 
+ 
+  if(roman.includes("VV")){
+    roman = roman.replace("VV","X");
+  }
+
+
+//LL
+  if(roman.includes("XXXX")){
+    roman = roman.replace("XXXX","XL");
+  }
+  
+  if(roman.includes("XLX")){
+    roman = roman.replace("XLX","L");
+  }
+
+//CC
+  if(roman.includes("LXL")){
+    roman = roman.replace("LXL","XC");
+  }
+
+  if(roman.includes("XCX")){
+    roman = roman.replace("XCX","C");
+  }
+  
+
+// DD
+  if(roman.includes("CCCC")){
+    roman = roman.replace("CCCC","CD");
+  }
+  
+  if(roman.includes("CDC")){
+    roman = roman.replace("CDC","D");
+  }
+
+// MM
+  if(roman.includes("DCD")){
+    roman = roman.replace("DCD","CM");
+  }
+
+  if(roman.includes("CMC")){
+    roman = roman.replace("CMC","M");
+  }
+
+} //fin bucle
+
+ return roman;
+}
+
+convertToRoman(36);
